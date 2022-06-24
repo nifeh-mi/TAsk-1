10b1793107f7827d0aa4ac559b2d3ab1fecf2b90
nifeh-mi <105551626+nifeh-mi@users.noreply.github.com>
2022-06-24 15:53:48 +0100

    First commit stage

---
+++ life.js
@@ -0,0 +1,12 @@
+let firstName = "Name: Ësther O. Olasupo";
+console.log(firstName);
+
+let height = "Height: 5ft";
+console.log(height);
+
+let country = "Country: Nigeria";
+console.log(country);
+
+
+
+alert("Esther O. Olasupo" + "\n"  +" 5ft " +  "\n"+ "Nigeria");
